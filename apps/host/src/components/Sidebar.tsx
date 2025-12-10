57cc9bd6bd4f2f3fa9b42fd7105705e7a60d74b5
Rafael Gomes <rafarfl2010@gmail.com>
2025-12-09 21:18:02 -0300

    feat: add responsive hamburger menu to sidebar

--- apps/host/src/components/Sidebar.tsx
+++ apps/host/src/components/Sidebar.tsx
@@ -1,11 +1,15 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { TiSocialLinkedin } from 'react-icons/ti'
 import { BiLogoPinterestAlt } from 'react-icons/bi'
 import { FaGithubAlt } from 'react-icons/fa6'
+import { IoIosMenu, IoMdClose } from 'react-icons/io'
 import profileImage from '../assets/profile.jpg'
 import brandImage from '../assets/brand.png'
 
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div>
       <div className="avatar">
@@ -18,6 +22,13 @@ export default function Sidebar() {
       </NavLink>
       <span className="role">Front-end Developer</span>
       <div className="social">
+        <button
+          className="menu-toggle"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label="Toggle navigation"
+        >
+          {isOpen ? <IoMdClose size={24} /> : <IoIosMenu size={24} />}
+        </button>
         <a
           href="https://www.linkedin.com/in/rflrafa/"
           aria-label="LinkedIn"
@@ -44,14 +55,23 @@ export default function Sidebar() {
           </span>
         </a>
       </div>
-      <nav className="nav">
-        <NavLink to="/" end>
+
+      <nav className={`nav ${isOpen ? 'open' : ''}`}>
+        <NavLink to="/" end onClick={() => setIsOpen(false)}>
           Inicial
         </NavLink>
-        <NavLink to="/projetos">Projetos</NavLink>
-        <NavLink to="/sobre-mim">Sobre mim</NavLink>
-        <NavLink to="/certificados">Certificados</NavLink>
-        <NavLink to="/laboratorio">Laboratório</NavLink>
+        <NavLink to="/projetos" onClick={() => setIsOpen(false)}>
+          Projetos
+        </NavLink>
+        <NavLink to="/sobre-mim" onClick={() => setIsOpen(false)}>
+          Sobre mim
+        </NavLink>
+        <NavLink to="/certificados" onClick={() => setIsOpen(false)}>
+          Certificados
+        </NavLink>
+        <NavLink to="/laboratorio" onClick={() => setIsOpen(false)}>
+          Laboratório
+        </NavLink>
       </nav>
     </div>
   )
